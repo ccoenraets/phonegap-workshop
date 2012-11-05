d2130fcd7eb4f2cc54468395b7844673ef100997
Christophe Coenraets <ccoenraets@gmail.com>
2012-11-05 14:32:44 -0800

    step 8

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 var app = {
 
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -10,6 +33,7 @@ var app = {
 
     initialize: function() {
         var self = this;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
             $('body').html(new HomeView(self.store).render().el);
         });
